d144d06706acbd42d7e8160485b7f0fb7a4b3848
Carl Abel <caweb.cph@gmail.com>
2023-02-22 13:38:59 +0100

    Random id display working.

--- src/components/PopularClass.js
+++ src/components/PopularClass.js
@@ -1,19 +1,33 @@
+import { useEffect, useState } from "react"
 import useFetch from "../hooks/useFetch"
+import { useNavigate } from "react-router-dom"
 
 export default function PopularClass() {
-  const { data } = useFetch("http://localhost:4000/api/v1/classes/1")
+  const { data } = useFetch("http://localhost:4000/api/v1/classes")
 
-  console.log(data)
+  const [selectedData, setSelectedData] = useState(null)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!data) return
+
+    setSelectedData(data[Math.floor(Math.random() * data.length)])
+  }, [data])
+  console.log(selectedData)
   return (
     <>
-      {data && (
-        <div className="PopularClass relative">
+      {selectedData && (
+        <div
+          onClick={() => navigate(`/classes/${selectedData?.id}`)}
+          className="PopularClass relative"
+        >
           <h2 className="absolute text-[28px] w-60 leading-10 text-white bottom-6 ml-5">
-            {data.className}
+            {selectedData.className}
           </h2>
           <img
             className="object-cover h-96 rounded-lg"
-            src={data.asset.url}
+            src={selectedData.asset.url}
             alt=""
           />
         </div>
