7a648642e69aaf8fdf83dd456c41d0c1adfc4e34
Carl Abel <caweb.cph@gmail.com>
2023-02-22 13:39:12 +0100

    navbar toggles updated

--- src/templates/Navbar.js
+++ src/templates/Navbar.js
@@ -1,8 +1,10 @@
 import { useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+import { Triangle, X, Menu } from "lucide-react"
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const location = useLocation()
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen)
@@ -14,14 +16,29 @@ export default function Navbar() {
 
   return (
     <div>
-      <div className="absolute left-3 mt-6">Triangle</div>
-      <div className="absolute right-3 mt-6" onClick={toggleMenu}>
-        Navbar
+      <div
+        className="absolute left-3 mt-6 cursor-pointer"
+        onClick={() => {
+          if (location.pathname === "/home") {
+            toggleMenu()
+          } else {
+            window.location.href = "/home"
+          }
+        }}
+      >
+        {location.pathname === "/home" ? (
+          <Triangle className="ml-4 text-gray-500" size={24} />
+        ) : (
+          <Triangle className="ml-4" size={24} />
+        )}
       </div>
+      <Menu className="absolute mr-4 right-3 mt-6" onClick={toggleMenu}>
+        Navbar
+      </Menu>
       {isMenuOpen && (
         <div className="fixed inset-0 bg-white z-50">
           <div className="absolute top-0 right-0 m-4">
-            <button onClick={toggleMenu}>Close</button>
+            <X className="mr-4 right-3 mt-6" onClick={toggleMenu}></X>
           </div>
           <ul className="text-lg flex flex-col items-center justify-center h-full">
             <li className="my-4">
