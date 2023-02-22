80abe4f13081b3957c6c9a54c1d3d2b7b5699b4c
Carl Abel <caweb.cph@gmail.com>
2023-02-22 16:19:28 +0100

    installing and updating slider

--- src/components/ClassesForYou.js
+++ src/components/ClassesForYou.js
@@ -1,12 +1,24 @@
 import useFetch from "../hooks/useFetch"
 import { useNavigate } from "react-router-dom"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 
 export default function ClassesForYou() {
   const { data } = useFetch("http://localhost:4000/api/v1/classes")
   const navigate = useNavigate()
+
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2.75, // Styling purposes.
+    slidesToScroll: 1.8, // If more classes gets added, change this accordingly.
+  }
+
   return (
-    <>
-      <div className="flex gap-3 overflow-y-auto pb-3 pr-3">
+    <div className="pb-3">
+      <Slider {...settings}>
         {data &&
           data.map((item) => (
             <div
@@ -15,14 +27,14 @@ export default function ClassesForYou() {
               key={item.id}
             >
               <img
-                className="h-32 w-[100%] object-cover rounded-xl shadow-sm"
+                className="h-32 w-[90%] object-cover rounded-xl shadow-sm"
                 src={item.asset.url}
                 alt=""
               />
               <h2>{item.className}</h2>
             </div>
           ))}
-      </div>
-    </>
+      </Slider>
+    </div>
   )
 }
